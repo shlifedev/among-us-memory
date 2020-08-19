7f78bd3451d2713550ad699ad88629d811c2db39
Zero is Black <49047211+shlifedev@users.noreply.github.com>
2020-08-19 13:30:40 +0900

    read player position

--- AmongUsMemory/PlayerData.cs
+++ AmongUsMemory/PlayerData.cs
@@ -8,16 +8,66 @@ namespace HamsterCheese.AmongUsMemory
     public class PlayerData
     {
         public PlayerControll Instance;
-         
+
         public IntPtr PlayerControl_GetData_Offset = IntPtr.Zero;
 
 
         private string playerInfoOffset = null;
         public IntPtr playerInfoOffset_ptr;
-
-
         public IntPtr offset_ptr;
         public string offset_str;
+
+        public Vector2 GetSyncPosition()
+        {
+            try
+            {
+                int _offset_vec2_position = 60;
+                int _offset_vec2_sizeOf = 8;
+                var netTransform = ((int)Instance.NetTransform + _offset_vec2_position).ToString("X");
+                var vec2Data= Cheese.mem.ReadBytes($"{netTransform}",_offset_vec2_sizeOf); // 주소로부터 8바이트 읽는다   
+                if (vec2Data != null && vec2Data.Length != 0)
+                {
+                    var vec2 = Utils.FromBytes<Vector2>(vec2Data);
+                    return vec2;
+                }
+                else
+                {
+                    return Vector2.Zero;
+                }
+            }
+
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return Vector2.Zero;
+            }
+        }
+        public Vector2 GetMyPosition()
+        {
+            try
+            {
+                int _offset_vec2_position = 80;
+                int _offset_vec2_sizeOf = 8;
+                var netTransform = ((int)Instance.NetTransform + _offset_vec2_position).ToString("X");
+                var vec2Data= Cheese.mem.ReadBytes($"{netTransform}",_offset_vec2_sizeOf); // 주소로부터 8바이트 읽는다  
+                if (vec2Data != null && vec2Data.Length != 0)
+                {
+                    var vec2 = Utils.FromBytes<Vector2>(vec2Data);
+                    return vec2;
+                }
+                else
+                {
+                    return Vector2.Zero;
+                }
+            }
+            catch
+            {
+                return Vector2.Zero;
+            }
+        }
+
+
 
         /// <summary>
         /// PlayerInfo 가져오기 
--- AmongUsMemory/Structures.cs
+++ AmongUsMemory/Structures.cs
@@ -64,4 +64,35 @@ namespace HamsterCheese.AmongUsMemory
         public byte IsDead;
         private UIntPtr _object;
     }
+
+    [System.Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Vector2
+    {
+        public float x,y;
+
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public bool IsZero()
+        {
+            if (this.x == 0 && this.y == 0) return true;
+            else return false;
+        }
+        public bool IsGarbage()
+        {
+            if (this.x == float.MaxValue && this.y == float.MaxValue) return true;
+            else return false;
+        }
+        public static Vector2 Zero
+        {
+            get
+            {
+                return new Vector2(0, 0);
+            }
+        }
+    }
 }
