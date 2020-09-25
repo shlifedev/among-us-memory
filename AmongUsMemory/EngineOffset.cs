d209ababfe2d68c3651513aac5bdce42ba62c159
Zero Is Black <49047211+shlifedev@users.noreply.github.com>
2020-09-25 15:53:26 +0900

    Update EngineOffset.cs

--- AmongUsMemory/EngineOffset.cs
+++ AmongUsMemory/EngineOffset.cs
@@ -2,16 +2,13 @@ namespace HamsterCheese.AmongUsMemory
 {
     public sealed class Pattern
     {
-        /// <summary>
-        /// Get PlayerControl
-        /// </summary>
-        public static string PlayerControl_Pointer = "GameAssembly.dll+DA5A84";  //GameAssembly.dll+E22AE8
-        public static string ShipStatus_Pointer = "GameAssembly.dll+DA5A50";
-        public static string AmongusClient_Pointer = "GameAssembly.dll+DA5ACC";
-        /// <summary>
-        /// Get PlayerControl.Get_Data();
-        /// </summary>
-        public static string PlayerControl_GetData = "55 8B EC 80 3D BD B0 ??";
+        
+        //9.22s
+        public static string PlayerControl_Pointer = "GameAssembly.dll+1468910";
+        public static string ShipStatus_Pointer = "GameAssembly.dll+146885C";
+        public static string AmongusClient_Pointer = "GameAssembly.dll+1468840";
+        public static string PlayerControl_GetData = "55 8B EC 80 3D 2A FC ??";
+
         
     }
 } 
