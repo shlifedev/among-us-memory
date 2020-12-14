389954a2e9d750ebf5895fa14b13f321f5627be1
CrispCrew <james158@live.ca>
2020-12-14 15:22:38 -0800

    Update EngineOffset.cs

--- AmongUsMemory/EngineOffset.cs
+++ AmongUsMemory/EngineOffset.cs
@@ -2,10 +2,10 @@ namespace HamsterCheese.AmongUsMemory
 {
     public sealed class Pattern
     { 
-        public static string PlayerControl_Pointer = "GameAssembly.dll+1BE11CC";
-        public static string ShipStatus_Pointer = "GameAssembly.dll+1BE114C";
-        public static string AmongusClient_Pointer = "GameAssembly.dll+1BE1074";
-        public static string PlayerControl_GetData = "55 8B EC 80 3D 12 40 ??"; 
+        public static string PlayerControl_Pointer = "GameAssembly.dll+1C57F7C";
+        public static string ShipStatus_Pointer = "GameAssembly.dll+1C56C38";
+        public static string AmongusClient_Pointer = "GameAssembly.dll+1C57F54";
+        public static string PlayerControl_GetData = "55 8B EC 80 3D A9 D1 ??"; 
     }
 } 
   
